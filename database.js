b61fa315ab629242db654fca78a79e98efb6db0f
sarthakraj726-hash <sarthakraj726@gmail.com>
2026-06-21 18:06:04 +0000

    fix: align database module dependency and add start script

--- database.js
+++ database.js
@@ -4,7 +4,7 @@ const path = require('path');
 const dbFolder = process.env.RAILWAY_VOLUME_MOUNT_PATH || '.';
 const dbPath = path.join(dbFolder, 'bot_data.db');
 
-const db = new Database(dbPath);
+const db = new Database('database.db');
 
 db.exec(`
     CREATE TABLE IF NOT EXISTS economy (
